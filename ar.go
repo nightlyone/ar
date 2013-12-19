05099ddeab61fe13a617584a2585e820d3382813
Ingo Oeser <nightlyone@googlemail.com>
2013-12-19 23:36:58 +0100

    Initial commit

---
+++ ar.go
@@ -0,0 +1,112 @@
+package ar
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"strconv"
+	"syscall"
+	"time"
+)
+
+const (
+	Magic     = "!<arch>\n"
+	FileMagic = "\x60\x0A"
+)
+
+type file struct {
+	name [16]uint8 // Filename in  ASCII
+
+}
+
+type fileInfo struct {
+	name         string
+	owner, group string
+	mode         os.FileMode
+	size         int64
+	mtime        time.Time
+}
+
+type CorruptArchive string
+
+func (c CorruptArchive) Error() string {
+	return "corrupt archive: " + string(c)
+}
+
+func getFileMode(s string) (filemode os.FileMode, err error) {
+	mode, err := strconv.ParseUint(s, 8, 32)
+	if err != nil {
+		return filemode, CorruptArchive(err.Error())
+	}
+
+	if os.FileMode(mode) != (os.FileMode(mode) & (os.ModePerm | syscall.S_IFMT)) {
+		return filemode, CorruptArchive("invalid file mode")
+	}
+
+	switch mode & syscall.S_IFMT {
+	case 0: // no file type sepcified, assume regular file
+	case syscall.S_IFREG: // regular file, nothing to add
+	default:
+		return filemode, CorruptArchive("only regular files supported")
+	}
+
+	return os.FileMode(mode) & os.ModePerm, nil
+}
+
+func readFileHeader(r io.Reader) (*fileInfo, error) {
+	fh := make([]byte, 60)
+	_, err := io.ReadFull(r, fh)
+	if err != nil {
+		return nil, err
+	}
+
+	if string(fh[58:58+2]) != FileMagic {
+		return nil, CorruptArchive("file magic \"" + FileMagic + "\" not found")
+	}
+
+	name := string(bytes.TrimSpace(fh[0:16]))
+	secs, err := strconv.ParseInt(string(bytes.TrimSpace(fh[16:16+12])), 10, 64)
+	if err != nil {
+		return nil, CorruptArchive(err.Error())
+	}
+
+	owner, err := strconv.Atoi(string(bytes.TrimSpace(fh[28 : 28+6])))
+	if err != nil {
+		return nil, CorruptArchive(err.Error())
+	}
+	group, err := strconv.Atoi(string(bytes.TrimSpace(fh[34 : 34+6])))
+	if err != nil {
+		return nil, CorruptArchive(err.Error())
+	}
+
+	filemode, err := getFileMode(string(bytes.TrimSpace(fh[40 : 40+8])))
+	if err != nil {
+		return nil, err
+	}
+
+	filesize, err := strconv.ParseInt(string(bytes.TrimSpace(fh[48:48+10])), 10, 64)
+	if err != nil {
+		return nil, CorruptArchive(err.Error())
+	}
+
+	fi := &fileInfo{
+		name:  name,
+		owner: strconv.Itoa(owner),
+		group: strconv.Itoa(group),
+		mtime: time.Unix(secs, 0),
+		mode:  filemode,
+		size:  filesize,
+	}
+
+	return fi, nil
+}
+
+func hasMagic(r io.Reader) bool {
+	m := make([]byte, len(Magic))
+	_, err := io.ReadFull(r, m)
+	if err != nil {
+		return false
+	}
+
+	return string(m) == Magic
+}
---
+++ ar_test.go
@@ -0,0 +1,119 @@
+package ar
+
+import (
+	"io"
+	"os"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+)
+
+var testCommon = "!<arch>\n" +
+	"debian-binary   1385068169  0     0     100644  4         `\n" +
+	"2.0\n" +
+	"control.tar.gz  1385068169  0     0     100644  0         `\n"
+
+// I really miss this...
+func timeMustParse(layout, value string) time.Time {
+	t, err := time.Parse(layout, value)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+var testCommonFileHeaders = []struct {
+	in   string
+	want *fileInfo
+	err  string
+}{
+	{
+		in: "debian-binary   1385068169  0     0     100644  4         `\n",
+		want: &fileInfo{
+			name:  "debian-binary",
+			owner: "0",
+			group: "0",
+			mtime: time.Unix(1385068169, 0),
+			mode:  os.FileMode(0100644) & os.ModePerm,
+			size:  4,
+		},
+	},
+	{
+		in: "debian-binary   1385068169  0     0     644     4         `\n",
+		want: &fileInfo{
+			name:  "debian-binary",
+			owner: "0",
+			group: "0",
+			mtime: time.Unix(1385068169, 0),
+			mode:  os.FileMode(0644),
+			size:  4,
+		},
+	},
+	{
+		in:  "debian-binary   1385068169  a     0     100644  4         `\n",
+		err: "corrupt archive: strconv.ParseInt: parsing \"a\": invalid syntax",
+	},
+	{
+		in:  "debian-binary   1385068169  0     b     100644  4         `\n",
+		err: "corrupt archive: strconv.ParseInt: parsing \"b\": invalid syntax",
+	},
+	{
+		in:  "debian-binary   1385068169  0     0     120644  4         `\n",
+		err: "corrupt archive: only regular files supported",
+	},
+	{
+		in:  "debian-binary   1385068169  0     0     220644  4         `\n",
+		err: "corrupt archive: invalid file mode",
+	},
+}
+
+func TestReadFileHeader(t *testing.T) {
+	for i, test := range testCommonFileHeaders {
+		got, err := readFileHeader(strings.NewReader(test.in))
+		switch {
+		case err == nil && test.err != "":
+			t.Errorf("%d: got no err, expected err %v", i, test.err)
+			continue
+		case err != nil && test.err != err.Error():
+			t.Errorf("%d: got err %q, expected err %q", i, err, test.err)
+			continue
+		case err == nil && test.err == "":
+			// no error as expected
+		case err != nil && test.err == err.Error():
+			t.Logf("%d: got expected error %q", i, err)
+			continue
+		}
+
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("%d: got %#v, expected %+v", i, got, test.want)
+		} else {
+			t.Logf("%d: got %#v", i, got)
+		}
+	}
+}
+
+var testMagic = []struct {
+	in   io.Reader
+	want bool
+}{
+	{
+		in:   strings.NewReader(Magic),
+		want: true,
+	},
+	{
+		in:   strings.NewReader(strings.Repeat("a", len(Magic))),
+		want: false,
+	},
+}
+
+func TestReadMagic(t *testing.T) {
+	for i, test := range testMagic {
+		got := hasMagic(test.in)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("%d: got %#v, expected %+v", i, got, test.want)
+		} else {
+			t.Logf("%d: got %#v", i, got)
+		}
+	}
+}
